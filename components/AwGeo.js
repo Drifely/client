4a47d20c10d5cad2442a8f44ed50fb710e35b3b2
idabagusangga <angga.idabagus@gmail.com>
2018-03-25 21:06:15 +0700

    ya

--- components/AwGeo.js
+++ components/AwGeo.js
@@ -1,6 +1,8 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
+import { Container } from 'native-base'
 
 // create a component
 class awGeo extends Component {
@@ -44,20 +46,48 @@ class awGeo extends Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>{JSON.stringify(this.state.gloc)}</Text>
-				<Text> SPEED: {Math.floor(this.state.gloc.coords.speed * 3.6)}</Text>
-			</View>
+
+			<Container style={styles.container}>
+				<MapView
+					style={styles.map}
+					region={{
+						latitude: this.state.gloc.coords.latitude || 37.78825,
+						longitude: this.state.gloc.coords.longitude || -122.4324,
+						latitudeDelta: 0.020,
+						longitudeDelta: 0.0121,
+					}}
+					>
+						<Marker
+							coordinate= {{latitude: this.state.gloc.coords.latitude || 37.7883, longitude: this.state.gloc.coords.longitude || -122.4324}}
+							title= "Test"
+							description= "Current Position" 
+							/>
+				</MapView>
+				<View style={styles.containerSpeed}>
+					<Text>{JSON.stringify(this.state.gloc)}</Text>
+					<Text> SPEED: {Math.floor(this.state.gloc.coords.speed * 3.6)}</Text>
+				</View>
+			</Container>
 		);
 	}
 }
 
 // define your styles
 const styles = StyleSheet.create({
-	container: {
+	containerSpeed: {
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	container: {
+		...StyleSheet.absoluteFillObject,
+		height: 700,
+		width: 400,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
 	},
 });
 
