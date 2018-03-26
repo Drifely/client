5ea4f9491eb504e2d8c8ebabca6155000507ea20
dorman99 <hafizh.abdillahh@gmail.com>
2018-03-26 16:45:16 +0700

    editing

--- components/SimInvalid.js
+++ components/SimInvalid.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import {  View, Text } from 'react-native';
-import {Button} from 'native-base'
+import {  View, Text, AsyncStorage} from 'react-native';
+import {
+	Button,
+	Container,
+	Header,
+	Content,
+	Left
+} from 'native-base'
+import { NavigationActions } from 'react-navigation'
 export default class InvalidSim extends Component {
+	goToMainScreen() {
+		AsyncStorage.clear()
+		const goTo = NavigationActions.reset({
+			index: 0,
+			actions: [NavigationActions.navigate({ routeName: 'Welcome' })]
+		})
+		this.props.navigation.dispatch(goTo);
+	}
 	render() {
-		retakepicture =  () => {
-			this.navigation.navigate('SnapSim')
-		}
 		return (
-			<View style={{alignItems:'center', justifyContent: 'center', flex:1}}>
-				<Text style={{fontWeight: 'bold'}}> InvalidSim </Text>
-				<Button 
-				success
-				bordered
-				onPress={()=> this.retakepicture()}> 
-					<Text>
-						Re-Take Picture
-					</Text>
-				</Button>
-			</View>
+			<Container> 
+				<Header/>
+				<Content contentContainerStyle={{ backgroundColor: 'indigo', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+				<View >
+				<Left >
+							<Text style={{ margin: 20, fontWeight: 'bold', color: 'white'}}> Invalid Sim-page !</Text>
+					<Left> 
+					<Button
+					onPress={() => this.goToMainScreen()} 
+					success
+					bordered><Text style={{fontWeight: 'bold', color:'white'}}> Re-Take Pic </Text></Button>
+					</Left>
+					</Left>
+				</View>
+				</Content>
+			</Container>
 		);
 	}
 }
